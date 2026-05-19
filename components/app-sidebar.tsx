6bebda02aff3130b6ba1a50baf52bf9cee72de6c
Mateus Fuzer <mateus_fuzer@outlook.com>
2026-05-18 23:53:09 -0300

    feat: Inicia a configuração do projeto

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,130 @@
+"use client"
+
+import * as React from "react"
+
+import { NavMain } from "@/components/nav-main"
+import { NavUser } from "@/components/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import { TerminalSquareIcon, BookOpenIcon, Settings2Icon, LifeBuoyIcon, SendIcon, FrameIcon, PieChartIcon, MapIcon, TerminalIcon, Tag, Settings, CalendarRange } from "lucide-react"
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Eventos",
+      url: "#",
+      icon: (
+        <CalendarRange
+        />
+      ),
+      isActive: true,
+      items: [
+        {
+          title: "Meus eventos",
+          url: "#",
+        }
+      ],
+    },
+    {
+      title: "Tags",
+      url: "#",
+      icon: (
+        <Tag
+        />
+      ),
+      items: [
+        {
+          title: "Minhas tags",
+          url: "#",
+        }
+      ],
+    },
+    {
+      title: "Configuração",
+      url: "#",
+      icon: (
+        <Settings
+        />
+      ),
+      items: [
+        {
+          title: "Login",
+          url: "#",
+        },
+        {
+          title: "WebHook",
+          url: "#",
+        }
+      ],
+    },
+     {
+      title: "Documentação",
+      url: "#",
+      icon: (
+        <BookOpenIcon
+        />
+      ),
+      items: [
+        {
+          title: "Introduction",
+          url: "#",
+        },
+        {
+          title: "Get Started",
+          url: "#",
+        },
+        {
+          title: "Tutorials",
+          url: "#",
+        },
+        {
+          title: "Changelog",
+          url: "#",
+        },
+      ],
+    }
+  ],
+  
+}
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar
+      className="top-(--header-height) h-[calc(100svh-var(--header-height))]!"
+      {...props}
+    >
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" render={<a href="#" />}>
+              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                <TerminalIcon className="size-4" />
+              </div>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-medium">Acme Inc</span>
+                <span className="truncate text-xs">Enterprise</span>
+              </div>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
